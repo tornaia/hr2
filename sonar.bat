bc2dfdc7fd07b62210bc83f437720ca458f08b46
tornaia <tornai.andras.ede@gmail.com>
2016-03-01 18:18:44 +0100

    cleanup remove unnecessary hr_environment variables. rely on defaults

--- sonar.bat
+++ sonar.bat
@@ -1,3 +1,3 @@
 SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 SET MAVEN_OPTS=-server -Xms1024m -Xmx1024m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:+DisableExplicitGC
-mvn -Dhr_environment=test clean test verify sonar:sonar
+mvn clean test verify sonar:sonar
