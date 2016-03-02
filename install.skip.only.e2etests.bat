c84441c440a0a3532e10d2c25127924e65c5982f
tornaia <tornai.andras.ede@gmail.com>
2016-03-02 20:12:54 +0100

    add skip-e2etest profile (by default nothing is skipped) add example batch file

---
+++ install.skip.only.e2etests.bat
@@ -0,0 +1,2 @@
+SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
+mvn -Pwithout-e2etest clean install
--- install.skip.tests.bat
+++ install.skip.tests.bat
@@ -1,2 +1,2 @@
 SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
-mvn -DskipTests -Dmaven.test.skip clean install
+mvn -DskipTests -Dmaven.test.skip -Pwithout-e2etest clean install
